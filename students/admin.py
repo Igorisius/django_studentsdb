3a940da284e0c00661c0c8bad393ca9e807db07f
Sakivskyy Igor <Sakivskyy.igor@gmail.com>
2016-03-03 23:47:56 +0200

    migrate on MySQL

--- students/admin.py
+++ students/admin.py
@@ -7,29 +7,33 @@ from django.forms import ModelForm, ValidationError
 from .models import Student, Group, Exam, MonthJournal
 
 class StudentFormAdmin(ModelForm):
-# get group where current student is a leader
-	def clean_student_group(self):
-		groups = Group.object.filter(leader=self.instance)
-		if len(groups) > 0 and self.cleaned_data['student_group'] != groups[0]:
-		    raise ValidationError(u'Студент є старостою іншої групи', code='invalid')
 
-		return self.cleaned_data['student_group']
-		
-		
+    def clean_student_group(self):
+        """Check if student is leader in any group.
+
+        If yes, then ensure it's the same as selected group."""
+        # get group where current student is a leader
+        groups = Group.objects.filter(leader=self.instance)
+        if len(groups) > 0 and self.cleaned_data['student_group'] != groups[0]:
+            raise ValidationError(
+                _(u"Student is a leader of a different group."),
+                code='invalid')
+
+        return self.cleaned_data['student_group']
 
 class StudentAdmin(admin.ModelAdmin):
-	list_display = ['last_name', 'first_name', 'ticket', 'student_group']
-	list_display_links = ['last_name', 'first_name']
-	list_editable = ['student_group']
-	ordering = ['last_name']
-	list_filter = ['last_name', 'first_name', 'middle_name', 'ticket']
-	list_per_page = 10
-	search_fields = ['last_name', 'first_name', 'middle_name', 'ticket', 'notes']
-	form = StudentFormAdmin
+    list_display = ['last_name', 'first_name', 'ticket', 'student_group']
+    list_display_links = ['last_name', 'first_name']
+    list_editable = ['student_group']
+    ordering = ['last_name']
+    list_filter = ['student_group']
+    list_per_page = 10
+    search_fields = ['last_name', 'first_name', 'middle_name', 'ticket',
+        'notes']
+    form = StudentFormAdmin
 
-	def view_on_site(self, obj):
-		return reverse('students_edit', kwargs={'pk': obj.id})
-
+    def view_on_site(self, obj):
+        return reverse('students_edit', kwargs={'pk': obj.id})
 
 class GroupAdmin(admin.ModelAdmin):
 	list_display = ['title', 'leader']
